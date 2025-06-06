9503c596e26e0f743a438cebdcffed54e2e7c48b
EdgarK7 <edgarukas505@gmail.com>
2025-06-06 07:12:16 -0700

    Added game window and food object

---
+++ Snake/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+Color green = { 173, 204, 96, 255 };
+Color darkgreen = { 43, 51, 24, 255 };
+
+int cellSize = 30;
+int cellCount = 25;
+
+class Food
+{
+public:
+	Vector2 position;
+	Texture2D texture;
+
+	Food()
+	{
+		Image image = LoadImage("Model/food.png");
+		texture = LoadTextureFromImage(image);
+		UnloadImage(image);
+		position = GenerateRandomePos();
+	}
+
+	~Food()
+	{
+		UnloadTexture(texture);
+	}
+
+	void Draw()
+	{
+		DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+	}
+
+	Vector2 GenerateRandomePos()
+	{
+		float x = GetRandomValue(0, cellCount - 1);
+		float y = GetRandomValue(0, cellCount - 1);
+		return Vector2{ x,y };
+	}
+};
+
+int main()
+{
+	cout << "Game" << endl;
+	InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake");
+	SetTargetFPS(60);
+
+	Food food = Food();
+
+	while (WindowShouldClose() == false)
+	{
+		BeginDrawing();
+
+		ClearBackground(green);
+		food.Draw();
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
